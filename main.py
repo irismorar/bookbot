6a6099afa6b508c7ddc7bccf8f3708ff91737aaa
Iris Adina Morar <morariris@gmail.com>
2024-06-18 19:08:02 +0300

    aggregate all the word and character data into a report

--- main.py
+++ main.py
@@ -2,17 +2,34 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
     num_chars = get_num_chars(text)
-    print(num_chars)
+    chars_sorted_list = get_chars_sorted_list(num_chars)
+    
+
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+
 def get_num_words(text):
     words = text.split()
     return len(words)
+
 
 def get_num_chars(text):
     chars = {}
@@ -23,6 +40,18 @@ def get_num_chars(text):
         else:
             chars[lowered] = 1
     return chars
+
+
+def sort_on(d):
+    return d["num"]
+
+def get_chars_sorted_list(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
 
 main()
 
