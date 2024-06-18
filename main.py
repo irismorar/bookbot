458ca7a64946cc18e8023c400214a679f54544d2
Iris Adina Morar <morariris@gmail.com>
2024-06-18 18:24:11 +0300

    create a new function in main.py file which takes the text from the book as a string and returns the number of times each character appears in the string

--- main.py
+++ main.py
@@ -3,17 +3,26 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     print(f"{num_words} words found in the document")
-
-
-def get_num_words(text):
-    words = text.split()
-    return len(words)
-
+    num_chars = get_num_chars(text)
+    print(num_chars)
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+def get_num_chars(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
 
 main()
 
